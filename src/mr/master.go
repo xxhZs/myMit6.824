82b1748d1c35626b4e0f296c9c95590d74fb515a
w1060434431 <1060434431@qq.com>
2021-12-01 21:50:32 +0800

    lab1 实现master ,没有锁和修改worker

--- src/mr/master.go
+++ src/mr/master.go
@@ -2,27 +2,40 @@ package mr
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
 import "net"
 import "net/rpc"
 import "net/http"
 
-var Jobid int = 0
+var mu sync.Mutex
 
 type JobType int
 
 const (
 	Map JobType = iota
 	Reduce
+	KillJob
+)
+
+type Status int
+
+const (
+	Start Status = iota
+	Using
+	End
 )
 
 type Job struct {
 	JobType     JobType
 	JobId       int
-	JobFileName string
-	ReduceId    int
+	JobFileName []string
 	ReduceNum   int
 }
 
@@ -31,7 +44,29 @@ type Master struct {
 	// 使用channel来存放
 	JobChannelMap    chan *Job
 	JobChannelReduce chan *Job
+	ReduceNum        int
+	MapNum           int
+	FirstJobId       int
+	MasterStatus     MasterStatus
+	JobMetaMap       JobMetaMap
 }
+
+//这个是所谓的job元数据，主要用来保存job的一些信息
+type JobMetaInfo struct {
+	startTime time.Time
+	status    Status
+	JobPtr    *Job
+}
+type JobMetaMap struct {
+	MetaMap map[int]*JobMetaInfo
+}
+type MasterStatus int
+
+const (
+	MapPhase = iota
+	ReducePhase
+	Waiting
+)
 
 // Your code here -- RPC handlers for the worker to call.
 
@@ -45,30 +80,44 @@ func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
 	return nil
 }
 func (m *Master) FromWorker(args *ExampleArgs, reply *Job) error {
-	fmt.Print(args.X)
-	*reply = *<-m.JobChannelMap
+	println("worker来领任务了")
+	switch m.MasterStatus {
+	case MapPhase:
+		if len(m.JobChannelMap) > 0 {
+			*reply = *<-m.JobChannelMap
+			if !m.JobMetaMap.sendMetaJob(reply.JobId) {
+				println("找个job开始运行了:", reply)
+			}
+		} else {
+			//说明任务全部完成了，要更新状态了
+			if m.JobMetaMap.checkJobNum() {
+				m.nextStatus()
+			}
+		}
+		break
+	case ReducePhase:
+		if len(m.JobChannelReduce) > 0 {
+			*reply = *<-m.JobChannelReduce
+			if !m.JobMetaMap.sendMetaJob(reply.JobId) {
+				println("找个job开始运行了:", reply)
+			}
+		} else {
+			//说明任务全部完成了，要更新状态了
+			if m.JobMetaMap.checkJobNum() {
+				m.nextStatus()
+			}
+		}
+		break
+	default:
+		reply.JobType = KillJob
+	}
 	return nil
 }
 
-func getJobId() (a int) {
-	a = Jobid
-	Jobid++
-	return a
-}
-func getReduceId() (a int) {
-	return 0
-}
-func (m *Master) MasterFindMap(filename []string, reduceNum int) {
-	for _, v := range filename {
-		job := Job{
-			JobType:     Map,
-			JobFileName: v,
-			ReduceId:    getReduceId(),
-			JobId:       getJobId(),
-			ReduceNum:   reduceNum,
-		}
-		m.JobChannelMap <- &job
-	}
+func (m *Master) getReduceId() (a int) {
+	jobId := m.FirstJobId
+	m.FirstJobId += 1
+	return jobId
 }
 
 //
@@ -108,6 +157,13 @@ func MakeMaster(files []string, nReduce int) *Master {
 	m := Master{
 		JobChannelMap:    make(chan *Job, len(files)),
 		JobChannelReduce: make(chan *Job, nReduce),
+		ReduceNum:        nReduce,
+		MapNum:           len(files),
+		FirstJobId:       0,
+		MasterStatus:     MapPhase,
+		JobMetaMap: JobMetaMap{
+			MetaMap: make(map[int]*JobMetaInfo, len(files)+nReduce),
+		},
 	}
 	m.MasterFindMap(files, nReduce)
 	m.server()
@@ -115,4 +171,178 @@ func MakeMaster(files []string, nReduce int) *Master {
 	// Your code here.
 
 	return &m
+}
+
+//map方法的封装
+func (m *Master) MasterFindMap(filename []string, reduceNum int) {
+	for _, v := range filename {
+		job := Job{
+			JobType:     Map,
+			JobFileName: []string{v},
+			JobId:       m.getReduceId(),
+			ReduceNum:   reduceNum,
+		}
+		jobMetaInfo := JobMetaInfo{
+			status: Start,
+			JobPtr: &job,
+		}
+		//将job元信息放入map
+		m.putMap(&jobMetaInfo)
+		//将job放入队列中
+		println("将job加入队列：", job)
+		m.JobChannelMap <- &job
+	}
+}
+
+//reduce
+func (m *Master) MasterFindReduce() {
+	for i := 0; i < m.ReduceNum; i++ {
+		job := Job{
+			JobType:     Reduce,
+			JobId:       m.getReduceId(),
+			JobFileName: getReduceFileName(i, "main"),
+		}
+		jobMetaInfo := JobMetaInfo{
+			status: Start,
+			JobPtr: &job,
+		}
+		//将job元信息放入map
+		m.putMap(&jobMetaInfo)
+		//将job放入队列中
+		println("将job加入队列：", job)
+		m.JobChannelMap <- &job
+	}
+}
+func (m *Master) putMap(jobMetaInfo *JobMetaInfo) bool {
+	jobId := jobMetaInfo.JobPtr.JobId
+	if m.JobMetaMap.MetaMap[jobId] == nil {
+		println("将job元信息", jobId, "加入队列")
+		m.JobMetaMap.MetaMap[jobId] = jobMetaInfo
+	} else {
+		println("这个job元信息已经存在：", jobId)
+		return false
+	}
+	return true
+}
+
+//更新状态
+func (j *JobMetaMap) sendMetaJob(jobId int) bool {
+	jobMetaInfo, err := j.MetaMap[jobId]
+	if !err || jobMetaInfo.status != Start {
+		println("这个job已经不在start", jobId)
+		return false
+	}
+	jobMetaInfo.startTime = time.Now()
+	jobMetaInfo.status = Using
+	return true
+}
+
+//master的状态变化,其实主要是map转换为reduce，以及reduce转换为waiting
+func (m *Master) nextStatus() {
+	if m.MasterStatus == MapPhase {
+		m.MasterFindReduce()
+		m.MasterStatus = ReducePhase
+	} else if m.MasterStatus == ReducePhase {
+		m.MasterStatus = Waiting
+	} else {
+		println("master状态不对")
+	}
+}
+
+/*
+* 通过reduceID来找到相对应的文件名
+ */
+func getReduceFileName(reduceId int, path string) []string {
+	rd, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println("错误：getReduceFileName打开目录错误", err.Error())
+		return nil
+	}
+	var res []string
+	for _, fi := range rd {
+		if !fi.IsDir() {
+			fiName := fi.Name()
+			if strings.HasPrefix(fiName, "mr-out-") && strings.HasSuffix(fiName, strconv.Itoa(reduceId)) {
+				fullName := path + fiName
+				res = append(res, fullName)
+			}
+		}
+	}
+	return res
+}
+func (j JobMetaMap) checkJobNum() bool {
+	mapNoDo := 0
+	mapDo := 0
+	reduceNoDo := 0
+	reduceDo := 0
+	for _, job := range j.MetaMap {
+		if job.JobPtr.JobType == Map {
+			if job.status == End {
+				mapDo += 1
+			} else {
+				mapNoDo += 1
+			}
+		} else {
+			if job.status == End {
+				reduceDo += 1
+			} else {
+				reduceNoDo += 1
+			}
+		}
+	}
+	return (mapDo > 0 && mapNoDo == 0) || (reduceDo > 0 && reduceNoDo == 0)
+}
+
+// 任务完成的方法
+func (m *Master) JobDone(args *Job, reply *ExampleReply) error {
+	switch args.JobType {
+	case Map:
+		job, OK := m.JobMetaMap.MetaMap[args.JobId]
+		if OK && job.status == Using {
+			job.status = End
+			println("job map已经完成：", job)
+		} else {
+			println("错误：job map已经完成或者没有开始", job)
+		}
+		break
+	case Reduce:
+		job, OK := m.JobMetaMap.MetaMap[args.JobId]
+		if OK && job.status == Using {
+			job.status = End
+			println("job reduce已经完成：", job)
+		} else {
+			println("错误：job reduce已经完成或者没有开始", job)
+		}
+		break
+	default:
+		println("错误： job错误")
+	}
+	return nil
+}
+
+//超时管理的方法
+func (m *Master) isTimeOut() {
+	for {
+		time.Sleep(time.Second)
+		if m.MasterStatus == Waiting {
+			break
+		}
+
+		for _, job := range m.JobMetaMap.MetaMap {
+			if job.status == Start {
+				fmt.Println("job依旧在运行", job, time.Now().Sub(job.startTime))
+			}
+			if job.status == Start && time.Now().Sub(job.startTime) > time.Second*2 {
+				fmt.Println("job超时了", job, time.Now().Sub(job.startTime))
+				if job.JobPtr.JobType == Map {
+					m.JobChannelMap <- job.JobPtr
+					job.status = Start
+				} else if job.JobPtr.JobType == Reduce {
+					m.JobChannelReduce <- job.JobPtr
+					job.status = Start
+				}
+			}
+
+		}
+	}
 }
